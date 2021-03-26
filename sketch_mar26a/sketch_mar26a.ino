f97981abf6c20e7bbc482bec084f1946903cf220
Marshellson <jiangyilun2000@outlook.com>
2021-03-26 12:39:33 +0100

    init

---
+++ sketch_mar26a/sketch_mar26a.ino
@@ -0,0 +1,13 @@
+// 一共两个马达， 两个传感器，前轮两个珠子 xD，后轮各两个马达 
+
+#define model_left
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
